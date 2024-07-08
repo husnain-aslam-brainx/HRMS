505be991b599acbc35ac8d9a12c9382292e7bf53
Husnain Aslam <husnain.aslam@brainxtech.com>
2024-07-08 18:55:33 +0500

    Add new flags

--- app/controllers/candidate_position_tests_controller.rb
+++ app/controllers/candidate_position_tests_controller.rb
@@ -9,22 +9,34 @@ class CandidatePositionTestsController < ApplicationController
         @positions = Position.all
     end
     def create 
-        @candidate_position_test = CandidatePositionTest.new(candidate_position_test_params)
+        candidate = Candidate.find(candidate_position_test_params[:candidate])
+        test = Test.find(candidate_position_test_params[:test])
+        position = Position.find(candidate_position_test_params[:position])
+        @candidate_position_test = CandidatePositionTest.create(candidate: candidate,test: test,position: position,status: candidate_position_test_params[:status],result: candidate_position_test_params[:result])
         if @candidate_position_test.save
             redirect_to candidate_position_tests_path
         else
             render :new
         end
     end
-    def edit 
+    def edit
         @candidate_position_test = CandidatePositionTest.find(params[:id])
-    end
-    def update 
+        @candidates = Candidate.all
+        @positions = Position.all
+        @tests = Test.all
+      end
+    def update
         @candidate_position_test = CandidatePositionTest.find(params[:id])
-        if @candidate_position_test.update(candidate_position_test_params)
+        candidate = Candidate.find(candidate_position_test_params[:candidate])
+        test = Test.find(candidate_position_test_params[:test])
+        position = Position.find(candidate_position_test_params[:position])
+        if @candidate_position_test.update(candidate: candidate,test: test,position: position,status: candidate_position_test_params[:status],result: candidate_position_test_params[:result])
             redirect_to candidate_position_tests_path
         else
-            render :edit
+          @candidates = Candidate.all
+          @positions = Position.all
+          @tests = Test.all
+          render :edit
         end
     end
     def destroy 
@@ -34,6 +46,6 @@ class CandidatePositionTestsController < ApplicationController
     end
 
     def candidate_position_test_params
-        params.require(:candidate_position_test).permit(:candidate_id, :test_id, :position_id, :status, :result)
+        params.require(:candidate_position_test).permit(:candidate, :test, :position, :status, :result)
     end
 end
