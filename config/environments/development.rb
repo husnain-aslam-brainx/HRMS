47718af6d8a5c3a2c0ba43e0ab3d98f5eb7f3d34
Husnain Aslam <husnain.aslam@brainxtech.com>
2024-07-10 18:29:10 +0500

    Submit form at changing position while creating interview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   config.assets.compile = true
+  config.assets.digest = false
 
 
   # Raises error for missing translations.
