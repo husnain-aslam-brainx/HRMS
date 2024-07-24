87b7816af71fd765ab3c56b7027ae9d033488011
Husnain Aslam <husnain.aslam@brainxtech.com>
2024-07-24 15:09:06 +0500

    Adds scope to interview model

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,7 @@
 class Interview < ApplicationRecord
+    scope :passed, ->{where(result: "passed")}
+    scope :for_position, ->(position_title){.joins(position).where(positions: {title: position_title})}
+    
     enum :interview_type, [:initial , :final]
     enum :result, [:passed, :failed]
     belongs_to :candidate_position_test
